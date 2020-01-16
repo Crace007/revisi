80fb417887026cea1d1ac0920b2158eefa9d3df9
Crace007 <fatwa.tkj1@gmail.com>
2020-01-16 12:20:20 +0700

    revisi

--- app/src/main/java/com/nim1718002/profile/MainActivity.java
+++ app/src/main/java/com/nim1718002/profile/MainActivity.java
@@ -1,8 +1,12 @@
 package com.nim1718002.profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.item_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.setting){
+            Intent a = new Intent(MainActivity.this, Setting.class);
+            startActivity(a);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
